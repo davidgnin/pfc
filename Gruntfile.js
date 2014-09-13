a1ddbfe359e787fb878576b1c2442a01355a92e5
David Gómez Nin <me@davidgnin.com>
2014-09-13 14:49:05 +0200

    New version of iconfont + grunt files

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+    },
+    copy: {
+    },
+    csslint: {
+    },
+    cssmin: {
+    },
+    htmlmin: {
+    },
+    imagemin: {
+    },
+    jshint: {
+    },
+    mincss: {
+    },
+    sass: {
+    },
+    uglify: {
+    },
+    watch: {
+    }
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-csslint");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-htmlmin");
+  grunt.loadNpmTasks("grunt-contrib-imagemin");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-mincss");
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  // Tasks
+  grunt.registerTask('default', ['uglify']);
+
+};
